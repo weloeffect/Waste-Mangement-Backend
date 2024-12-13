63067e64273c9bfe06f071dac71613c63a2b4b9a
sajid <sajidbrong@gmail.com>
2024-12-13 14:59:40 +0100

    update the swagger configurations

--- src/main/java/com/example/garbagecollection/config/SecurityConfig.java
+++ src/main/java/com/example/garbagecollection/config/SecurityConfig.java
@@ -1,16 +1,29 @@
 package com.example.garbagecollection.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@SecurityScheme(
+        type = SecuritySchemeType.HTTP,
+        name = "basicAuth",
+        scheme = "basic",
+        in = SecuritySchemeIn.HEADER
+)
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)) // Allow H2 Console frames
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/swagger-ui/**",
@@ -21,9 +34,9 @@ public class SecurityConfig {
                         ).permitAll() // No login required for these paths
                         .anyRequest().authenticated() // Secure other endpoints
                 )
-                .csrf(csrf -> csrf.ignoringRequestMatchers("/console/**")) // Disable CSRF for H2 Console
-                .headers(headers -> headers.frameOptions().disable()) // Allow H2 Console frames
-                .httpBasic(); // Use basic authentication for other endpoints
+                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF for H2 Console
+//                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults()); // Use basic authentication for other endpoints
 
         return http.build();
     }
